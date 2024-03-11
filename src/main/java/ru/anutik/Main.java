07b425aa417b65d2b350e24375dcd6e19055224e
SilverySound <162194875+SilverySound@users.noreply.github.com>
2024-03-12 01:54:52 +0300

    Update Main.java

--- src/main/java/ru/anutik/Main.java
+++ src/main/java/ru/anutik/Main.java
@@ -2,7 +2,7 @@ package ru.anutik;
 
 public class Main
 {
-    private static String msg =""Hello qa_guru!!!!!!!!"";
+    private static String msg ="Hello qa_guru!";
     public static void main(String[] args)
     {
         System.out.println(msg);
