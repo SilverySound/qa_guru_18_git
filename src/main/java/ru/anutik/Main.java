a4175e7aee42abcb8abf1d2ff06e15c03434bf5a
Anna Shatokhina <anutik_94.08@mail.ru>
2024-03-12 01:43:46 +0300

    added getMessage() method

--- src/main/java/ru/anutik/Main.java
+++ src/main/java/ru/anutik/Main.java
@@ -4,6 +4,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello qa_guru!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage(){
+        return "Hello qa_guru!";
     }
 }
